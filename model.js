9086bbf8819d5926576a0898ce1def9ef08445ef
buan0001 <123996422+buan0001@users.noreply.github.com>
2024-09-23 15:40:38 +0200

    has game of life algos that shouldn't be here teehee

--- model.js
+++ model.js
@@ -1,8 +1,8 @@
-export { init, writeToCell, readFromCell, dump };
+export { init, writeToCell, readFromCell, dump,scanForNeighbours };
 
 // const grid =[[],[],[""]]
-const grid =[[0,0,0],[0,0,0],[0,0,0]]
-// const grid =[[1,2,3],[4,5,6],[7,8,9]]
+// const grid =[[0,0,0],[0,0,0],[0,0,0]]
+const grid =[[1,2,3],[4,5,6],[7,8,9]]
 
 function init() {
   console.log("Model is running");
@@ -26,4 +26,33 @@ function dump() {
     //     console.log(row);
         
     // }
+}
+
+
+function scanForNeighbours(cellRow, cellCol){
+    let neighbours = 0;
+    for (let x = -1; x <= 1; x++) {
+        for (let y = -1; y <= 1; y++) {
+            // console.log("Row: " + x + " col: " + y  + " Val: "+ grid[cellRow+x][cellCol+y]);            
+            if (!(x == 0 && y == 0)){
+                const tempVal =readFromCell(cellRow - x, cellCol -y)
+                console.log("Temp val:",tempVal);
+                
+                if (tempVal) neighbours++;
+            }
+        }
+        
+    }
+    console.log("neighbours:", neighbours);
+    
+}
+
+function liveOrDie(neighbours){
+    if (neighbours < 2 || neighbours > 3){
+        // tile = 0
+    }
+    else if (neighbours == 3) {
+        // tile = 1
+    }
+    // do nothing
 }
